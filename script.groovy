12d907d4de133b6588ce62c5d98b515513b9f17a
Rohit1247 <90217302+Rohit1247@users.noreply.github.com>
2021-10-30 15:22:21 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,17 +2,18 @@
     echo "Building the application..."
     sh 'mvn package'
     }
-    def buildImage(){
+    def buildImage() {
 
-    echo "Building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t rohit1247/newimage:jma2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push rohit1247/newimage:jma2.0'
+        echo "Building the docker image..."
+        withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            sh 'docker build -t rohit1247/newimage:jma2.0 .'
+            sh "echo $PASS | docker login -u $USER --password-stdin"
+            sh 'docker push rohit1247/newimage:jma2.0'
+        }
     }
     def deployApp(){
 
         echo "Deploying the application..."
     }
-    }
+    
 return this
